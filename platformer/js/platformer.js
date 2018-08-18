cf45b36e36737d6eb6cebaf82f201e28134ac9ee
Marcus Sanatan <msanatan@gmail.com>
2018-08-18 16:26:54 -0400

    Implement gravity with player

--- platformer/js/platformer.js
+++ platformer/js/platformer.js
@@ -3,15 +3,52 @@ kontra.init();
 
 let player = kontra.sprite({
   x: 5,
-  y: kontra.canvas.height - 10,
+  y: kontra.canvas.height - 30,
   color: 'red',
   width: 10,
   height: 6,
+  gravity: 0.75,
+  grounded: false,
+  jumping: false,
+  lastY: 0,
 });
+
+
+let collideGround = () => {
+  if (player.y >= kontra.canvas.height - player.height) {
+      player.y = kontra.canvas.height - player.height;
+      player.grounded = true;
+      player.jumping = false;
+  }
+}
 
 let loop = kontra.gameLoop({
   update: () => {
     player.update();
+    collideGround();
+
+    // Determine when to stop jumping if not colliding with anything
+    if (player.lastY - player.y >= 40) {
+      player.jumping = false;
+    }
+
+    // Pull player down if not jumping and in the air
+    if (!player.grounded && !player.jumping) {
+      player.y += player.gravity;
+      player.dy = 0;
+    }
+
+    // Pick up player movement
+    if (kontra.keys.pressed('space') && player.grounded) {
+      player.dy = -player.gravity - 0.25;
+      player.grounded = false;
+      player.jumping = true;
+      player.lastY = player.y;
+    } else if (kontra.keys.pressed('right')) {
+      player.x += 1;
+    } else if (kontra.keys.pressed('left')) {
+      player.x -= 1;
+    }
   },
 
   render: () => {
